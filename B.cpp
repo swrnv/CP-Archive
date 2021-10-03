08a6eb0a65ce90ad898d0b1c90866d6c115197b8
Aviansh <56665437+AvinashSthaz@users.noreply.github.com>
2021-10-03 13:43:57 +0545

    Add files via upload

--- B.cpp
+++ B.cpp
@@ -1,53 +1,76 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define ar array
 #define ll long long
-
-const int MAX_N = 1e5 + 1;
 const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
+const int MAX_N=1e6;
 
 
+void solve() {
+	int n;
+	cin >>  n;
+	int ar[n];
+	int ar2[n];
+	map<int,int> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> ar[i];
+		m[ar[i]]=i;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> ar2[i];
+		m[ar2[i]]=i;
+	}
+	//cout  << m[2] << "SAsf" << endl;
+    int ans=MAX_N,max=-1;
+    int cnt=0;
+	for (int i = 0; i < n; ++i)
+	{
+		if(ar2[i]>max){
+			for (int j=ar2[i]; j!=max && j>1 ;j-=2 )
+			{
+				//cout << j-1 << "\n";
+				int pos=m[j-1]+i;
+				ans=min(ans,pos);
+			}
+			max=ar2[i];}
+	}
+	//cout <<"----------------------"  << "\n";
+	cout << ans << "\n";
+// 	int pos=-1;
+// 	int pos2=-1;
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		if(ar[i])
+// 	}
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		if(ar[i]<ar2[0])
+// 		{
+// 			pos2=i;break;
+// 		}
+// 	}
+// cout << min(pos,pos2) << "\n";
 
 
+}
 
 int main() {
+    auto begin = std::chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    int tc = 1,n,a,b;
+    int tc=1;
     cin >> tc;
-    for (int t = 1; t <= tc; t++) {
-        //cin >> n >> a >> b;
-        int ar[n];
-        int sum=0;
-        for (int i = 0; i < n; ++i)
-        {
-           // cin >> ar[i];
-            sum+=(i+1)*ar[i];
-        }
-        int v[22];
-        v[0]=0;
-        v[1]=1;
-
-        for (int i = 2; i < 22; ++i)
-        {
-            v[i]=v[i-1]+v[i-2];
-        }
-
-        for (int i = 0; i < 22; ++i)
-        {
-            cout << v[i] << "\n";
-        }
-
-
-        // for(auto it:v)
-        //     cout << it  << " ";
-
-
-        
-         cout << "Case #" << t  << ": " << sum-1 << endl;
-       
-    }
+    while(tc--) solve();
+    auto end = std::chrono::high_resolution_clock::now();
+        cout << setprecision(4) << fixed;
+        //cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
 }
+
+
+// 1
+// 5
+// 7 3 5 9 1
+// 2 4 6 10 8
+// 3   // ans should be 2 while output is 3!
